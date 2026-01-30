fbf7c10fec7a679ca284a7a4024f036c30d007eb
mod-kaif07 <mohammadkaif6660@gmail.com>
2026-01-30 22:24:11 +0530

    Remove unused Redux Toolkit project files

--- 01ReduxUnderstanding/src/Redux/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import CounterSlicereducer  from "./CounterSlice";
-
-export const Store = configureStore({
-  reducer: {
-    Counter:CounterSlicereducer
-  },
-});
